d9580fb9fcc9f3fe5f9b6eb876ec93367db4787d
Usman Ghani <ugkhan2@gmail.com>
2017-08-01 18:22:55 +0300

    initial

--- js/main.js
+++ js/main.js
@@ -1,39 +1,5 @@
 $(document).ready(function() {
 
-	// slider functions
-	(function () {
-		$first = $('#first');
-		$second = $('#second');
-		$third = $('#third');
-		$currentBubble = $("#first");
-		$current = $('.slide-show img:visible');
-
-		$('.slide-show img:gt(0)').hide();
-		$("#first").css('background-color', '#393939');
-		setInterval(function () {
-			current = $('.slide-show img:visible');
-			$next = current.next().length? current.next():$('.slide-show img:eq(0)');
-			$nextBubble = $currentBubble.next().length? $currentBubble.next(): $('#first');
-			$current.fadeOut(1000);
-			$currentBubble.css('background-color', 'white');
-			$next.fadeIn(1000);
-			$nextBubble.css('background-color', '#393939');
-			$currentBubble=$nextBubble;
-		},3000);
-
-		$first.mouseover(bubbleHover);
-		$second.mouseover(bubbleHover);
-		$third.mouseover(bubbleHover);
-		function bubbleHover() {
-			$('.slide-show img').hide();
-			$('.bullets span:eq(index)').css('background-color', '#393939');
-			$('.bullets span:gt(index)').css('background-color', 'white');
-			$('.slide-show img:eq(index)').fadeIn();
-			$currentBubble=$(this);
-			current = $('.slide-show img:visible');
-		}
-	}());
-
 	// rollover Effect in Membership HTML.
 	(function () {
 		$package2 = $('#package2'); // Silver Package
@@ -47,26 +13,23 @@ $(document).ready(function() {
 
 		function rollover(){
 			$(this).addClass('rollover')
-			$(this).children('.v-div').css({
-				'border-top': '15px solid #318921',
+			$(this).children('.v-div').css({   // v-div adds triangle on packages over hover.
+				'border-top': '15px solid #1b7410',
 				'border-left': $(this).width()/2 +'px solid transparent',
 				'border-right': $(this).width()/2 +'px solid transparent'
 			});
-			$(this).children('h4').css({
-				'background-color': '#318921',
-				'border-radius': '3px'
-			});
+			$(this).children('h4').addClass('package-heading-hover');
+			$(this).children('.btn').addClass('btn-hover');
 		}
 		function rollout(){
 			$(this).removeClass('rollover')
 			$(this).children('.v-div').css('border-top', '15px solid transparent');
-			$(this).children('h4').css({
-				'background-color': '#23546B',
-				'border-radius':'0'
-			});
+			$(this).children('h4').removeClass('package-heading-hover');
+			$(this).children('.btn').removeClass('btn-hover');
 		}
 	}());
 
+	// changing media icon images on hover.
 	(function () {
 		$mediaIcons = $('.icon');
 		$mediaIcons.hover(function () {
